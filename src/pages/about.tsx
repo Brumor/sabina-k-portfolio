6b0cb32c76f44917cf1c302594e6e385b954b03f
Brumor <paulbricout2@gmail.com>
2020-05-14 22:21:44 +0200

    Done with about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,6 +8,8 @@ import { connect } from "react-redux"
 import { fetchCategoriesData } from "../state/actions"
 import { bindActionCreators, AnyAction, Dispatch } from "redux"
 
+import styles from "./about.module.scss"
+
 interface IProps {
   categories: Category[]
   fetchCategoriesData: () => void
@@ -18,9 +20,9 @@ const Gallery: FC<IProps> = ({ categories, fetchCategoriesData }) => {
 
   const urlParams = new URLSearchParams(queryString)
 
-  const category = urlParams.get("category")
+  const categoryName = urlParams.get("category")
 
-  console.log({ category, categories })
+  console.log({ categoryName, categories })
 
   useEffect(() => {
     if (categories === null) {
@@ -30,10 +32,37 @@ const Gallery: FC<IProps> = ({ categories, fetchCategoriesData }) => {
 
   return (
     <Layout>
-      <SEO title="Gallery" />
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
+      <SEO title="Sabina Kovacevic" />
+      <div className={styles.container}>
+        <h1>Sabina Kovacevic</h1>
+        <img
+          className={styles.aboutMeImage}
+          src="https://drive.google.com/uc?id=1Kwgz8blkVpHMsijlbAwmALVD_OKvCMH6&export=download"
+        />
+        <h2>Jag är en stor potatis</h2>
+        <p className={styles.aboutMeText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          finibus cursus porta. Aliquam eget pulvinar orci, id vulputate diam.
+          Proin eget pretium sapien, et molestie dui. Donec in libero a arcu
+          varius feugiat. Vestibulum varius tempor vehicula. Quisque et pretium
+          quam. Cras interdum, tellus vehicula feugiat volutpat, justo nunc
+          porttitor enim, sed porttitor magna risus eget elit. Ut vel velit
+          metus. Proin tempus id risus a congue.
+          <br />
+          <br />
+          Proin tempus massa ut finibus ultrices. Pellentesque viverra, lacus
+          non eleifend vehicula, lacus sem rhoncus mauris, eget viverra leo
+          sapien eu ipsum. Donec in justo vehicula, scelerisque ipsum non,
+          lacinia odio. Fusce eget ex feugiat, fringilla justo in, mattis justo.
+          Maecenas quis sem eu nisl tincidunt lacinia. Aenean consequat
+          malesuada quam in consequat. Donec finibus blandit tincidunt. Mauris
+          congue tristique lacus quis laoreet. Sed vel nibh vel nunc lacinia
+          vulputate. Aenean at facilisis sem. Sed suscipit consectetur posuere.
+          Donec laoreet dapibus consequat. Etiam sit amet tempus lorem, a
+          pulvinar justo. Aenean malesuada sapien iaculis nibh euismod, non
+          sodales nulla hendrerit.
+        </p>
+      </div>
     </Layout>
   )
 }
