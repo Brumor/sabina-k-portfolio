5bf292ec46fb38b9bfeb262e670597792c2c5af4
Brumor <paulbricout2@gmail.com>
2020-05-16 20:37:07 +0200

    Temporary fix for category  page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,26 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { navigate } from "gatsby"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  useEffect(() => {
+    const location = window.location.pathname
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const categoryName = urlParams.get("category")
+    if (location.split("/").indexOf("gallery") !== -1 && categoryName !== "") {
+      navigate(`/`)
+    }
+  }, [])
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
