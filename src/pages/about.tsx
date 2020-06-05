0a3ecc77c61fea2007d14c8521a12c63c7dfdf64
Brumor <paulbricout2@gmail.com>
2020-06-05 17:37:23 +0200

    About me page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -21,28 +21,30 @@ const Gallery: FC<IProps> = () => {
       <div className={styles.container}>
         <h1>Sabina Kovacevic</h1>
         <img className={styles.aboutMeImage} src={Sabina} />
-        <h2>Jag är en stor potatis</h2>
+        <h2>Amatörfotograf bosatt i Arvika</h2>
         <p className={styles.aboutMeText}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          finibus cursus porta. Aliquam eget pulvinar orci, id vulputate diam.
-          Proin eget pretium sapien, et molestie dui. Donec in libero a arcu
-          varius feugiat. Vestibulum varius tempor vehicula. Quisque et pretium
-          quam. Cras interdum, tellus vehicula feugiat volutpat, justo nunc
-          porttitor enim, sed porttitor magna risus eget elit. Ut vel velit
-          metus. Proin tempus id risus a congue.
+          En värmländsk 18-åring med en särskild kärlek för att fota porträtt
+          och konserter, ja det beskriver nog mig ganska bra! Jag fick upp
+          ögonen för fotografering när jag fick en systemkamera i
+          födelsedagspresent för ett par år sedan och sedan dess har den varit
+          en självklar del av min vardag. Från det att jag började fotografera
+          har jag experimenterat en hel del och därmed utvecklats mycket i mitt
+          bildspråk, och i dagsläget så fotar jag med en Nikon D750.
           <br />
           <br />
-          Proin tempus massa ut finibus ultrices. Pellentesque viverra, lacus
-          non eleifend vehicula, lacus sem rhoncus mauris, eget viverra leo
-          sapien eu ipsum. Donec in justo vehicula, scelerisque ipsum non,
-          lacinia odio. Fusce eget ex feugiat, fringilla justo in, mattis justo.
-          Maecenas quis sem eu nisl tincidunt lacinia. Aenean consequat
-          malesuada quam in consequat. Donec finibus blandit tincidunt. Mauris
-          congue tristique lacus quis laoreet. Sed vel nibh vel nunc lacinia
-          vulputate. Aenean at facilisis sem. Sed suscipit consectetur posuere.
-          Donec laoreet dapibus consequat. Etiam sit amet tempus lorem, a
-          pulvinar justo. Aenean malesuada sapien iaculis nibh euismod, non
-          sodales nulla hendrerit.
+          Erfarenhetsmässigt så var jag fotograf för Dansstudions vårshow 2019,
+          jag var en av fotograferna för UKM i Arvika 2020 och jag har fotat ett
+          flertal studentkonserter. Jag har inte varit anställd för någon
+          porträttfotografering men jag har mycket erfarenhet av porträttfoto,
+          vilket man kan se om man klickar in i{" "}
+          <Link to="/gallery?category=Porträtt">den mappen</Link>.
+          <br />
+          <br />
+          Men egentligen, så länge jag har en kamera och ett motiv så är jag
+          nöjd! Jag kan tänka mig att fota allt, och om du tycker om mitt
+          bildspråk och är intresserad så tveka inte att höra av dig! Klicka in
+          på <Link to="/gallery?category=Porträtt">kontaktsidan</Link> så hittar
+          du alla mina uppgifter där.
         </p>
       </div>
     </Layout>
